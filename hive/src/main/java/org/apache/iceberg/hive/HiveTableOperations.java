dc75a7efc0e11be72c6597b9f63c557f9aca7c45
Anton Okolnychyi <aokolnychyi@apple.com>
2019-07-08 09:24:36 +0100

    Add PDT properties

--- hive/src/main/java/org/apache/iceberg/hive/HiveTableOperations.java
+++ hive/src/main/java/org/apache/iceberg/hive/HiveTableOperations.java
@@ -168,6 +168,7 @@ public class HiveTableOperations extends BaseMetastoreTableOperations {
             baseMetadataLocation, metadataLocation, database, tableName);
       }
 
+      setPdtParameters(metadata.properties(), tbl);
       setParameters(newMetadataLocation, tbl);
 
       if (base != null) {
@@ -208,6 +209,18 @@ public class HiveTableOperations extends BaseMetastoreTableOperations {
       }
       unlock(lockId);
     }
+  }
+
+  private void setPdtParameters(Map<String, String> tableProperties, Table tbl) {
+    // We need to persist `pdt.` and `spark.sql.sources.` properties in HMS for PDT tables
+    Map<String, String> parameters = tbl.getParameters();
+    List<String> existingPdtParams = parameters.keySet().stream()
+        .filter(key -> key.startsWith("pdt.") || key.startsWith("spark.sql.sources."))
+        .collect(Collectors.toList());
+    existingPdtParams.forEach(parameters::remove);
+    tableProperties.entrySet().stream()
+        .filter(entry -> entry.getKey().startsWith("pdt.") || entry.getKey().startsWith("spark.sql.sources."))
+        .forEach(entry -> parameters.put(entry.getKey(), entry.getValue()));
   }
 
   private void setParameters(String newMetadataLocation, Table tbl) {
